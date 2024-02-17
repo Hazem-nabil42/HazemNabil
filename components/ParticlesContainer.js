8b03e80fbe3e3d4a1822a47d2bcb19af288cc538
Hazemnabil_42 <124460984+Hazem-nabil42@users.noreply.github.com>
2024-02-17 14:49:38 +0200

    Update ParticlesContainer.js

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -12,7 +12,7 @@ const ParticlesContainer = () => {
 
   return(
     <Particles 
-      className='w-full h-full absolute translate-z-0'
+      className='w-full h-full absolute translate-z-0 hidden md:flex'
       id='tsparticles' 
       init={particlesInit} 
       loaded={particlesLoaded}
