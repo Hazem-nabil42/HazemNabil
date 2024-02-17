c17b4fe24b8493efdd75a7b28f9a99536061f599
Hazemnabil_42 <124460984+Hazem-nabil42@users.noreply.github.com>
2024-02-17 14:09:12 +0200

    Update index.js

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -47,7 +47,7 @@ const Contact = () => {
               max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center
               overflow-hidden hover:border-accent group '>
               <span className='group-hover:-translate-y-[120%]
-              group-hover:opacity-0 transition-all duration-500'> Let's talk </span>
+              group-hover:opacity-0 transition-all duration-500'> Lets talk </span>
               <BsArrowRight 
                 className='-translate-y-[120%] opacity-0 group-hover:flex
                 group-hover:-translate-y-0 group-hover:opacity-100 transition-all
