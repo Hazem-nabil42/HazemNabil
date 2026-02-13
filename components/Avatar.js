a4ea11d59771a19ae02683e7f7b79c5b1ecfcdfa
Hazemnabil_42 <124460984+Hazem-nabil42@users.noreply.github.com>
2026-02-13 21:37:23 +0200

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -6,10 +6,10 @@ const Avatar = () => {
   return <div className='hidden xl:flex xl:max-w-none'>
     <Image 
     src='https://hazem-nabil42.github.io/HazemNabil/me2.jpeg'
-    width={637}
-    height={578}
+    width={410} 
+    height={400} 
     alt='HazemNabil'
-    className='translate-z-0 w-full h-full rounded-[50%] shadow-2xl
+    className='translate-z-0 w-full h-full object-cover rounded-full shadow-2xl
     shadow-red-200 opacity-65'
   
     />
